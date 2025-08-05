4559b02a512dd7b52dd76248b11e642210c24ab6
sam stone <51040430+samstone512@users.noreply.github.com>
2025-08-05 16:38:00 +0330

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ def load_secrets() -> dict:
     """Loads all necessary secrets from the Colab environment."""
     return {
         "telegram": userdata.get('telegram'),
-        "gemini_api_key": userdata.get('GOOGLE_API_KEY'), # <--- تغییر کرد
+        "gemini_api_key": userdata.get('GOOGLE_API_KEY'), 
         "google_creds": userdata.get('Calendar_credentials'),
         "notion_key": userdata.get('NOTION_API_KEY'),
         "notion_ideas_db_id": userdata.get('NOTION_IDEAS_DB_ID'),
